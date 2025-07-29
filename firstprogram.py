fa84012325139e8b4bd9e559a3e721af3cd049b1
sonamwangmo <seezyweezy19@gmail.com>
2025-07-29 14:35:12 +0600

    my name

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,9 @@
 print("hello world")
 print("hello SWE2025")
+a=2
+b=3
+sum = a+b
+print("The sum of a and b is:",sum)
+name = input("Enter your name:")
+age = input("Enter your age")
+print("You are",name, "and you are",age,"years old")
