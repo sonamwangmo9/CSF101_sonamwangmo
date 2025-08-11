12db9f1407464b4de939a09b827ba895201f7f6a
sonamwangmo <seezyweezy19@gmail.com>
2025-08-12 00:29:53 +0600

    exercise 2.3.1

---
+++ csf101-python_exercises/operators/comparion.py
@@ -0,0 +1,8 @@
+a, b = 10, 5
+print(f"a = {a}, b = {b}")
+print(f"a == b: {a == b}")
+print(f"a != b: {a != b}")
+print(f"a > b: {a > b}")
+print(f"a < b: {a < b}")
+print(f"a >= b: {a>= b}")
+print(f"a <= b: {a <= b}")
