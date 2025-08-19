03d353984b74ca25c73acd852768dd151485e1cf
sonamwangmo <seezyweezy19@gmail.com>
2025-08-19 14:54:05 +0600

    Practical 3

---
+++ csf101-python_exercises/practical 3/implementing a stack.py
@@ -0,0 +1,28 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise len(self.items)
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Stack is empty") 
+    def size(self):
+        return len(self.items)
+           
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.pop())
+print(stack.peek())
+print(stack.size())
